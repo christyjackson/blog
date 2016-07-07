cfde06b738f8eaf3f5003b4b8c547c651f08be5c
Christy Jackson <jchristyjackson@gmail.com>
2016-07-07 22:54:31 +0530

    Add edit and update for projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -21,6 +21,23 @@ class ProjectsController < ApplicationController
 
 	end
 
+	def edit
+	end
+
+	def update
+		if @project.update project_params
+			redirect_to @project,notice: "Nice Jackson! That project was succesfully updated."
+		else
+			render 'edit'
+		end
+
+	end
+
+	def destroy
+		@project.destroy
+		redirect_to projects_path
+	end
+
 	private
 
 	def find_project
